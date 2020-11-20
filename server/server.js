4d40d346990667073fc49ec2ff95459ad24c5be3
Kristiina Eelnurme <kristiina.eelnurme@gmail.com>
2020-11-20 17:19:15 -0500

    Created route handlers and started coding risk algo

--- server/server.js
+++ server/server.js
@@ -6,14 +6,28 @@ const PORT = 3000;
 
 app.use(express.json());
 
-// handle post get hre:
-
 // statically serve everything in the build folder on the route '/build'
 app.use('/build', express.static(path.join(__dirname, '../build')));
 // serve index.html on all the pages
 app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
+
+// route handlers:
+//  will receive the Submit event from the frontend when user completes the quiz:
+app.post('/', quizController.getResponse, quizController.calculateRisk, (req, res) => {
+  res.status(200)
+    .redirect('/results');
+});
+
+// route handler to send risk assessment results back to client
+// app.get('/results', quizController.sendResult, (req, res) => {
+//   res.status(200)
+//     .send(somethinggoeshere);
+// });
+
+
+
 // global error handler
 app.use((err, req, res, next) => {
   console.log(err);
@@ -24,3 +38,5 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
+
+module.exports = app;
