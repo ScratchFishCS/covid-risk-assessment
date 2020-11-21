620551356b1e16286e745b7339b8ab3de1eb3d61
Jonah Stewart <jonahlstewart@gmail.com>
2020-11-20 22:15:29 -0500

    rendered nested components, added initial styling

---
+++ client/components/AboutWindow.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutWindow(props) {
+  return (
+    <div className="about-window">
+      <h3>About Window</h3>
+      <p>Within this paragraph, we will describe our app.</p>
+    </div>
+  );
+}
+
+export default AboutWindow;
--- client/components/AssessmentPage.jsx
+++ client/components/AssessmentPage.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import AssessmentWindow from './AssessmentWindow.jsx';
+import AboutWindow from './AboutWindow.jsx';
+// import styles from '../styles/styles.css';
 
 function AssessmentPage(props) {
+  const redirectToResults = () => {
+    return <Redirect to="/results" />;
+  };
+
   return (
     <div>
       <h1>Assessment Page</h1>
+      <div id="assessment-page">
+        <div className="window">
+          <AboutWindow />
+        </div>
+        <div className="window">
+          <AssessmentWindow />
+          <button
+            onClick={(e) => {
+              redirectToResults();
+            }}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
