8b7b1dc42530d5a6e5e96d465282cc8c6b145c7c
Jonah Stewart <jonahlstewart@gmail.com>
2020-11-21 15:35:15 -0500

    completed results page functionality and question shuffle, question lookup

---
+++ client/components/Activities.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Activities(props) {
+  return (
+    <div className="activity">
+      <p>{props.behavior}</p>
+    </div>
+  );
+}
+
+export default Activities;
--- client/components/AssessmentWindow.jsx
+++ client/components/AssessmentWindow.jsx
@@ -1,41 +1,9 @@
 import React, { Redirect } from 'react';
 import QuestionDisplay from './QuestionDisplay.jsx';
+import Questions from '../questions';
 
 function AssessmentWindow(props) {
-  // question bank
-  let questions = [
-    { text: 'Opened the mail', keyword: 'mail' },
-    { text: 'Got restaurant takeout', keyword: 'takeout' },
-    { text: 'Pumped gasoline', keyword: 'gas' },
-    { text: 'Played tennis', keyword: 'tennis' },
-    { text: 'Went camping', keyword: 'camp' },
-    { text: 'Went grocery shopping', keyword: 'grocery' },
-    { text: 'Went for a walk, run, or bike ride with other', keyword: 'walk' },
-    { text: 'Ate at a restaurant outside', keyword: 'restaurantOut' },
-    { text: "Sat in a doctor's waiting room", keyword: 'doctor' },
-    { text: 'Walked in a busy downtown', keyword: 'downtown' },
-    { text: "Had a meal at someone else's house", keyword: 'house' },
-    { text: 'Attended a backyard barbeque', keyword: 'bbq' },
-    { text: 'Shopped at a mall', keyword: 'mall' },
-    { text: 'Sent kids to school, camp, or day care', keyword: 'kids' },
-    {
-      text: 'Visited an elderly relative or friend in their home',
-      keyword: 'elderly',
-    },
-    { text: 'Went to a hair salon or barbershop', keyword: 'hair' },
-    { text: 'Ate at a restaurant inside', keyword: 'restaurantIn' },
-    { text: 'Traveled by plane', keyword: 'plane' },
-    { text: 'Attended a wedding or a funeral', keyword: 'wedding' },
-    { text: 'Hugged or shook hands when greeting a friend', keyword: 'hug' },
-    { text: 'Worked out at a gym', keyword: 'gym' },
-    { text: 'Went to a movie theatre', keyword: 'movie' },
-    { text: 'Attended a large music event', keyword: 'music' },
-    {
-      text: 'Attended an indoor religious or cultural event',
-      keyword: 'religious',
-    },
-    { text: 'Went to a bar', keyword: 'bar' },
-  ];
+  let questions = Questions;
 
   // question display generator
   questions = questions.map((question, index) => {
@@ -54,7 +22,7 @@ function AssessmentWindow(props) {
 
   return (
     <div className="assessment-window">
-      <h3>Assessment Window</h3>
+      <h3>Take The Assessment</h3>
       {questions}
     </div>
   );
